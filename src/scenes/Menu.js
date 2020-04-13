99e9026ce0623af68bce73f189234c2ad91ba2b7
Mark <markmdvd@gmail.com>
2020-04-13 14:33:25 -0700

    fixed file lookup.

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,7 +7,7 @@ class Menu extends Phaser.Scene {
         // load audio
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
-        this.load.audio('sfx_rocket', '../assets/rocket_shot.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
     }
 
     create() {
